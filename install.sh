0a4dae59bfd08f8890514b881bf3303bab922dbe
Kevin Mershon <kevin@mershon.enterprises>
2015-12-01 11:18:52 -0800

    Download mybb software and error handling

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "TBD"
+
+echo "Downloading myBB software"
+wget http://resources.mybb.com/downloads/mybb_1806.zip -O mybb.zip
+if [ $? != "0" ]; then
+    echo "Failed to download myBB software. Please try again later."
+    exit 1
+fi
